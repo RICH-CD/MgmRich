364b6655150632d6faddf9c8bd9f75e781714053
RICH-CD <cardenas.ricardo94@gmail.com>
2020-10-16 00:51:19 -0500

    inclusion de bootstrap

---
+++ src/components/Construir.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Construir extends Component{
+    constructor(){
+        super();
+        this.state = {
+                title: "Aplicacion de tareas",
+                ntareas: 10   
+        }
+    }
+}
+
+export default Construir;
